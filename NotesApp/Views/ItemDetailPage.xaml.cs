5ac08843a04599f5fc8782cbda717d075ba6f7e0
Tazwinator <taylor_stevenson@yahoo.co.uk>
2021-10-21 15:59:54 +0100

    Add project files.

---
+++ NotesApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using NotesApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace NotesApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Note Saving App
+
+My Note Saving App that uses Azure SQL Server and a user account feature to save notes to a user's account.
